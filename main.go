3944e4d7a2338c5433e140291a137cbf4b23a866
lllverb <54735495+lllverb@users.noreply.github.com>
2019-12-21 19:11:44 +0900

    html rendering

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"strconv"
+	// "strconv"
+
+	"html/template"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type Todo struct {
+// type Todo struct {
+// 	gorm.Model
+// 	Text   string
+// 	Status string
+// }
+
+type Quiz struct {
 	gorm.Model
-	Text   string
-	Status string
+	Quiz   string
+	Answer string
 }
 
 //DB初期化
@@ -20,69 +28,69 @@ func dbInit() {
 	if err != nil {
 		panic("データベース開けず！（dbInit）")
 	}
-	db.AutoMigrate(&Todo{})
+	db.AutoMigrate(&Quiz{})
 	defer db.Close()
 }
 
 //DB追加
-func dbInsert(text string, status string) {
+func create(quiz string, answer string) {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
 		panic("データベース開けず！（dbInsert)")
 	}
-	db.Create(&Todo{Text: text, Status: status})
+	db.Create(&Quiz{Quiz: quiz, Answer: answer})
 	defer db.Close()
 }
 
-//DB更新
-func dbUpdate(id int, text string, status string) {
-	db, err := gorm.Open("sqlite3", "test.sqlite3")
-	if err != nil {
-		panic("データベース開けず！（dbUpdate)")
-	}
-	var todo Todo
-	db.First(&todo, id)
-	todo.Text = text
-	todo.Status = status
-	db.Save(&todo)
-	db.Close()
-}
+// //DB更新
+// func dbUpdate(id int, text string, status string) {
+// 	db, err := gorm.Open("sqlite3", "test.sqlite3")
+// 	if err != nil {
+// 		panic("データベース開けず！（dbUpdate)")
+// 	}
+// 	var todo Todo
+// 	db.First(&todo, id)
+// 	todo.Text = text
+// 	todo.Status = status
+// 	db.Save(&todo)
+// 	db.Close()
+// }
 
-//DB削除
-func dbDelete(id int) {
-	db, err := gorm.Open("sqlite3", "test.sqlite3")
-	if err != nil {
-		panic("データベース開けず！（dbDelete)")
-	}
-	var todo Todo
-	db.First(&todo, id)
-	db.Delete(&todo)
-	db.Close()
-}
+// //DB削除
+// func dbDelete(id int) {
+// 	db, err := gorm.Open("sqlite3", "test.sqlite3")
+// 	if err != nil {
+// 		panic("データベース開けず！（dbDelete)")
+// 	}
+// 	var todo Todo
+// 	db.First(&todo, id)
+// 	db.Delete(&todo)
+// 	db.Close()
+// }
 
 //DB全取得
-func dbGetAll() []Todo {
+func dbGetAll() []Quiz {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
 		panic("データベース開けず！(dbGetAll())")
 	}
-	var todos []Todo
-	db.Order("created_at desc").Find(&todos)
+	var quizzes []Quiz
+	db.Order("created_at desc").Find(&quizzes)
 	db.Close()
-	return todos
+	return quizzes
 }
 
-//DB一つ取得
-func dbGetOne(id int) Todo {
-	db, err := gorm.Open("sqlite3", "test.sqlite3")
-	if err != nil {
-		panic("データベース開けず！(dbGetOne())")
-	}
-	var todo Todo
-	db.First(&todo, id)
-	db.Close()
-	return todo
-}
+// //DB一つ取得
+// func dbGetOne(id int) Todo {
+// 	db, err := gorm.Open("sqlite3", "test.sqlite3")
+// 	if err != nil {
+// 		panic("データベース開けず！(dbGetOne())")
+// 	}
+// 	var todo Todo
+// 	db.First(&todo, id)
+// 	db.Close()
+// 	return todo
+// }
 
 func main() {
 	router := gin.Default()
@@ -92,66 +100,78 @@ func main() {
 
 	//Index
 	router.GET("/", func(ctx *gin.Context) {
-		todos := dbGetAll()
-		ctx.HTML(200, "index.html", gin.H{
-			"todos": todos,
+		quizzes := dbGetAll()
+
+		html := template.Must(template.ParseFiles("templates/base.html", "templates/index.html"))
+		router.SetHTMLTemplate(html)
+		ctx.HTML(200, "base.html", gin.H{
+			"quizzes": quizzes,
+		})
+	})
+	router.GET("/sample", func(ctx *gin.Context) {
+		quizzes := dbGetAll()
+
+		html := template.Must(template.ParseFiles("templates/base.html", "templates/sample.html"))
+		router.SetHTMLTemplate(html)
+		ctx.HTML(200, "base.html", gin.H{
+			"quizzes": quizzes,
 		})
 	})
 
 	//Create
 	router.POST("/new", func(ctx *gin.Context) {
-		text := ctx.PostForm("text")
-		status := ctx.PostForm("status")
-		dbInsert(text, status)
+		quiz := ctx.PostForm("quiz")
+		answer := ctx.PostForm("answer")
+		create(quiz, answer)
 		ctx.Redirect(302, "/")
 	})
 
-	//Detail
-	router.GET("/detail/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic(err)
-		}
-		todo := dbGetOne(id)
-		ctx.HTML(200, "detail.html", gin.H{"todo": todo})
-	})
+	// Detail
+	// router.GET("/detail/:id", func(ctx *gin.Context) {
+	// 	n := ctx.Param("id")
+	// 	id, err := strconv.Atoi(n)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	todo := dbGetOne(id)
+	// 	ctx.HTML(200, "detail.html", gin.H{"todo": todo})
+	// })
 
-	//Update
-	router.POST("/update/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("ERROR")
-		}
-		text := ctx.PostForm("text")
-		status := ctx.PostForm("status")
-		dbUpdate(id, text, status)
-		ctx.Redirect(302, "/")
-	})
+	// Update
+	// router.POST("/update/:id", func(ctx *gin.Context) {
+	// 	n := ctx.Param("id")
+	// 	id, err := strconv.Atoi(n)
+	// 	if err != nil {
+	// 		panic("ERROR")
+	// 	}
+	// 	text := ctx.PostForm("text")
+	// 	status := ctx.PostForm("status")
+	// 	dbUpdate(id, text, status)
+	// 	ctx.Redirect(302, "/")
+	// })
 
-	//削除確認
-	router.GET("/delete_check/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("ERROR")
-		}
-		todo := dbGetOne(id)
-		ctx.HTML(200, "delete.html", gin.H{"todo": todo})
-	})
+	// 削除確認
+	// router.GET("/delete_check/:id", func(ctx *gin.Context) {
+	// 	n := ctx.Param("id")
+	// 	id, err := strconv.Atoi(n)
+	// 	if err != nil {
+	// 		panic("ERROR")
+	// 	}
+	// 	todo := dbGetOne(id)
+	// 	ctx.HTML(200, "delete.html", gin.H{"todo": todo})
+	// })
 
-	//Delete
-	router.POST("/delete/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("ERROR")
-		}
-		dbDelete(id)
-		ctx.Redirect(302, "/")
+	// Delete
+	// router.POST("/delete/:id", func(ctx *gin.Context) {
+	// 	n := ctx.Param("id")
+	// 	id, err := strconv.Atoi(n)
+	// 	if err != nil {
+	// 		panic("ERROR")
+	// 	}
+	// 	dbDelete(id)
+	// 	ctx.Redirect(302, "/")
 
-	})
+	// })
 
 	router.Run()
 }
